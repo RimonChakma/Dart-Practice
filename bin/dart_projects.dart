26294d96c85be01001c8e9bc8edc70a45262ee04
RimonChakma <rimon.chakma880@gmail.com>
2025-06-12 10:40:06 +0600

    Named parameter

--- bin/dart_projects.dart
+++ bin/dart_projects.dart
@@ -1,6 +1,18 @@
-void main () {
-  count(5, 5);
+void defaultValue() {
+  count(num1: 5, num2: 5);
 }
-count (int num1 ,int num2){
-  print(num1*num2);
+count ({required int num1, required int num2, int num3 =1}){
+  print(num1*num2 + num3);
+}
+
+void nullCheckUse () {
+  count1(num1: 5, num2: 5);
+}
+count1 ({required int num1, required int num2, int? num3}){
+  print(num1*num2 + (num3 ?? 0));
+}
+
+main(){
+  defaultValue();
+  nullCheckUse();
 }
--- bin/function/function.dart
+++ bin/function/function.dart
@@ -7,6 +7,26 @@ count (int num1 ,int num2){
   print(num1*num2);
 }
 
+//2. Name parameter
+
+void nullCheckUse () {
+  count1(num1: 5, num2: 5);
+}
+count1 ({required int num1, required int num2, int? num3}){
+  print(num1*num2 + (num3 ?? 0));
+}
+
+void defaultValue() {
+  count3(num1: 4, num2: 5);
+}
+count3({required int num1, required int num2, int num3 =2}){
+  print(num1*num2 + num3);
+}
+
+
+
 main(){
   task1();
+  nullCheckUse();
+  defaultValue();
 }
